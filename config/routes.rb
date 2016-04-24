61d0a818a853c9e82fde710dd9d3368d04277216
hpjaj <hmlevine@gmail.com>
2016-04-24 11:55:50 -0600

    Creates Post show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :show]
 
   resources :kids, only: [:index, :new, :create]
 end
