b7086dc38afa28241ca8214890981d88ac81fb4e
hpjaj <hmlevine@gmail.com>
2016-05-14 14:45:03 -0600

    Dynamically creates links to each of your kids' post pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def my_kids(current_user)
+    kids = current_user.try(:kids)
+
+    if kids.present? && kids.first.id.present?
+      kids
+    else
+      false
+    end
+  end
 end
