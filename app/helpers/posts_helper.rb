6a0f5f535d10cedc3418cc7f2919ec11e7ba2b29
hpjaj <hmlevine@gmail.com>
2016-05-20 15:50:34 -0600

    Adds more default choices for date_said

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,14 @@
 module PostsHelper
   def timing_choices
     now        = Date.current
+    yesterday  = now - 1.day
     this_week  = now.beginning_of_week
     last_week  = this_week - 1.week
     last_month = now.beginning_of_month - 1.month
 
     [
+      ['Today', "#{now}"],
+      ['Yesterday', "#{yesterday}"],
       ['This week', "#{this_week}"],
       ['Last week', "#{last_week}"],
       ['Last month', "#{last_month}"],
