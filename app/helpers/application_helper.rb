74879ad2cd72966e95b24690154b03cff28475db
hpjaj <hmlevine@gmail.com>
2016-05-20 15:54:25 -0600

    Adds sidebar navigation for Kids

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,8 @@ module ApplicationHelper
   def your_kids
     current_user.kids.present? ? current_user.kids.order(:name) : nil
   end
+
+  def your_friends_and_family
+    current_user.following.present? ? current_user.following.order(:name) : nil
   end
 end
