80ec1a139abc14345c410a3d3b733ef75c6c9bf0
hpjaj <hmlevine@gmail.com>
2016-05-29 22:15:25 -0600

    Duplicated validation

--- app/models/friend_and_family.rb
+++ app/models/friend_and_family.rb
@@ -5,7 +5,6 @@ class FriendAndFamily < ActiveRecord::Base
   validates :kid_id, presence: true
   validates :follower_id, presence: true
   validates :follower_id, uniqueness: { scope: [:kid_id, :follower_id], message: 'This friend/family member already has a permission for this kid' }
-  validate :cannot_create_duplicate_records, on: :create
 
   def self.parents_kids_friends_and_family(parent)
     kids_ids = parent.kids.pluck(:id)
@@ -15,14 +14,6 @@ class FriendAndFamily < ActiveRecord::Base
       .joins(:follower)
       .order('users.last_name')
       .order('users.first_name')
-  end
-
-  private
-
-  def cannot_create_duplicate_records
-    if FriendAndFamily.where(kid_id: self.kid_id, follower_id: self.follower_id).present?
-      errors.add(:follower_id, "This friend/family member already has a permission for this kid")
-    end
   end
 
 end
