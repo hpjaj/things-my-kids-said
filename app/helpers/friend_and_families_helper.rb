b8509317ebc4ceffbfe4e6930e2e42b70be0d541
hpjaj <hmlevine@gmail.com>
2016-06-22 16:02:52 -0600

    Refines delete message language

--- app/helpers/friend_and_families_helper.rb
+++ app/helpers/friend_and_families_helper.rb
@@ -2,4 +2,12 @@ module FriendAndFamiliesHelper
   def yes_or_no(boolean)
     boolean ? 'Yes' : 'No'
   end
+
+  def friends_name(friend)
+    User.find(friend.follower_id).full_name
+  end
+
+  def kids_name(friend)
+    Kid.find(friend.kid_id).first_name.titleize
+  end
 end
