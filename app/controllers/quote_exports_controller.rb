0c5d6f2f79088a092cf059b5c616d1a724c33ee6
hpjaj <hmlevine@gmail.com>
2016-06-22 22:20:08 -0600

    Orders the export by kid_id and created_at

--- app/controllers/quote_exports_controller.rb
+++ app/controllers/quote_exports_controller.rb
@@ -1,6 +1,6 @@
 class QuoteExportsController < ApplicationController
   def my_kids
-    @posts = Post.parents_can_see(current_user)
+    @posts = Post.parents_can_see(current_user).order(:kid_id).order('created_at DESC')
 
     send_data @posts.to_csv, filename: "things-my-kids-said-#{Date.today}.csv"
   end
