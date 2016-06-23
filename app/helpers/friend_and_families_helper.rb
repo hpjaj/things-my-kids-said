09fa32e9120f4cf6133c51dcb87e2935eacdb4d9
hpjaj <hmlevine@gmail.com>
2016-06-23 16:10:14 -0600

    Creates button and mailer to invite new members

--- app/helpers/friend_and_families_helper.rb
+++ app/helpers/friend_and_families_helper.rb
@@ -1,4 +1,5 @@
 module FriendAndFamiliesHelper
+
   def yes_or_no(boolean)
     boolean ? 'Yes' : 'No'
   end
@@ -10,4 +11,25 @@ module FriendAndFamiliesHelper
   def kids_name(friend)
     Kid.find(friend.kid_id).first_name.titleize
   end
+
+  def all_kids_have_said(current_user)
+    all_kids = current_user.kids
+
+    if all_kids.count == 1
+      all_kids.first.first_name.titleize + "has said"
+    elsif all_kids.count == 2
+      "#{all_kids.first.first_name.titleize} and #{all_kids.first.first_name.titleize} have said"
+    elsif all_kids.count > 2
+      all_kids = all_kids.to_a
+      last_kid = all_kids.pop
+      preamble = ""
+
+      all_kids.each do |kid|
+        preamble << "#{kid.first_name.titleize}, "
+      end
+
+      preamble + "and #{last_kid.first_name.titleize} have said"
+    end
+  end
+
 end
