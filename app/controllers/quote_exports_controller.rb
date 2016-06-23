cb5bf6688230e2e8cd2bce3bfb09bad78122124b
hpjaj <hmlevine@gmail.com>
2016-06-22 22:31:18 -0600

    Authenticates export controller

--- app/controllers/quote_exports_controller.rb
+++ app/controllers/quote_exports_controller.rb
@@ -1,6 +1,10 @@
 class QuoteExportsController < ApplicationController
+  before_action :authenticate_user!
+
   def my_kids
     @posts = Post.parents_can_see(current_user).order(:kid_id).order('created_at DESC')
+
+    authorize! :read, @posts.first
 
     send_data @posts.to_csv, filename: "things-my-kids-said-#{Date.today}.csv"
   end
