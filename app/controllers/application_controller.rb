ce1808e0684f1cd9e2ca368ca8b8a68dd6e5c082
hpjaj <hmlevine@gmail.com>
2016-07-05 20:28:37 -0600

    Adds Role to User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :role]
   end
 
 end
