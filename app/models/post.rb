c80595f2c9098315b127c9c0bd97f9070dcd8d42
hpjaj <hmlevine@gmail.com>
2016-07-23 09:49:58 -0600

    Preserving file keeps photo around upon deletion
    Preserving file allows for photo to not be deleted when either the user, or post, or kid is deleted.
    
    http://stackoverflow.com/questions/4911191/preventing-paperclip-from-deleting-overwriting-attachments-on-update

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    preserve_files: true
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   has_many :friend_and_families, foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :friend_and_families, source: :kid
 
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", preserve_files: true
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   validates :first_name, presence: true
