1a09b72ab6d3832e4f0309d3e07addd83a8361c4
hpjaj <hmlevine@gmail.com>
2016-07-30 20:01:19 -0600

    Temporary admin abilities

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,9 @@ class WelcomeController < ApplicationController
   end
 
   def home
-    if current_user
+    if current_user.admin?
+      @posts = Post.all.paginate(:page => params[:page], :per_page => 60)
+    elsif current_user
       @posts = Post.all_associated_kids_posts(current_user).paginate(:page => params[:page], :per_page => 20)
     else
       flash[:notice] = 'Please sign in to access this page'
