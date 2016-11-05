07f191aa93553b22bced8003d9ebb6975b525d55
Harry Levine <hmlevine@gmail.com>
2016-11-05 09:52:01 -0600

    Comments out irrelevant, error causing migration

--- db/migrate/20160701185746_add_photo_to_current_posts.rb
+++ db/migrate/20160701185746_add_photo_to_current_posts.rb
@@ -1,12 +1,12 @@
 class AddPhotoToCurrentPosts < ActiveRecord::Migration
   def up
-    Kid.all.each do |kid|
-      kid.posts.each do |post|
-        next if post.photo?
+    # Kid.all.each do |kid|
+    #   kid.posts.each do |post|
+    #     next if post.photo?
 
-        post.update(photo: kid.photo)
-      end
-    end
+    #     post.update(photo: kid.photo)
+    #   end
+    # end
   end
 
   def down
